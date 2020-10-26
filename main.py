14c853b39737c05bad6ce7f8d32b281575513952
Mitchell Adam <mbadam@ualberta.ca>
2020-10-26 15:30:53 -0700

    Add exit

--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 from sys import argv
 import database as db
 
-from utils import split_and_strip, request_input, print_invalid_input
+from utils import (
+    split_and_strip,
+    request_input,
+    print_invalid_input,
+    print_invalid_option
+)
 
 def logged_in(uid, pwd):
     print('Now logged in, to log out please type `logout`')
@@ -44,58 +49,63 @@ def logged_in(uid, pwd):
             pass
 
 def login():
-    print('To exit program enter CTRL + C') #TODO: Is this an ok way to exit? Probably not...
+    print('To exit program enter `exit` at any point')
     print('Enter:')
     print('1 for Login')
     print('2 for Sign-up')
-    action = request_input()[0]
+    while (True):
+        action = request_input()[0]
 
-    #Login
-    if action == '1':
-        print("To return to main screen enter `back`")
-        print('Enter: Id, password')
-        while(True):
-            login_values = request_input() #TODO: Should not show password
-            if login_values[0] == "back":
-                return False, None, None
+        #Login
+        if action == '1':
+            print("To return to main screen enter `back`")
+            print('Enter: Id, password')
+            while(True):
+                login_values = request_input() #TODO: Should not show password
+                if login_values[0] == "back":
+                    return False, None, None
 
-            if len(login_values) == 2:
-                # Attempt to login
-                login_success = db.login(
-                    login_values[0], login_values[1]
-                )
-                if login_success:
-                    return True, login_values[0], login_values[1]
+                if len(login_values) == 2:
+                    # Attempt to login
+                    login_success = db.login(
+                        login_values[0], login_values[1]
+                    )
+                    if login_success:
+                        # Check if user is privileged
+                        is_privileged = db.check_privilege(login_values[0])
+                        return True, login_values[0], login_values[1]
+                    else:
+                        print("Login failed, please try again")
                 else:
-                    print("Login failed, please try again")
-            else:
-                print_invalid_input()
+                    print_invalid_input()
 
 
-    #Sign up
-    elif action == '2':
-        print("To return to main screen enter `back`")
-        print('Enter: Id, Name, City, Password')
-        while(True):
-            sign_up_values = request_input() #TODO: Should not show password
-            if sign_up_values[0] == "back":
-                return False, None, None
+        #Sign up
+        elif action == '2':
+            print("To return to main screen enter `back`")
+            print('Enter: Id, Name, City, Password')
+            while(True):
+                sign_up_values = request_input() #TODO: Should not show password
+                if sign_up_values[0] == "back":
+                    return False, None, None
 
-            if len(sign_up_values) == 4:
-                # Attempt to sign up
-                sign_up_success = db.sign_up(
-                    sign_up_values[0],
-                    sign_up_values[1],
-                    sign_up_values[2],
-                    sign_up_values[3]
-                )
+                if len(sign_up_values) == 4:
+                    # Attempt to sign up
+                    sign_up_success = db.sign_up(
+                        sign_up_values[0],
+                        sign_up_values[1],
+                        sign_up_values[2],
+                        sign_up_values[3]
+                    )
 
-                if sign_up_success:
-                    return True, sign_up_values[0], sign_up_values[3]
+                    if sign_up_success:
+                        return True, sign_up_values[0], sign_up_values[3]
+                    else:
+                        print("Sign up failed, please try again")
                 else:
-                    print("Sign up failed, please try again")
-            else:
-                print_invalid_input()
+                    print_invalid_input()
+        else:
+            print_invalid_option()
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,13 +1,21 @@
-def split_and_strip(str_):
-    return [x.strip() for x in str_.split(',')]
+from sys import exit
+
+def split_and_strip(input_val):
+    return [x.strip() for x in str(input_val).split(',')]
 
 
 def request_input():
-    print('')
     response =  input('Input: ')
     print('')
+    values = split_and_strip(response)
 
-    return split_and_strip(response)
+    if values[0] == "exit":
+        exit()
+
+    return values
 
 def print_invalid_input():
     print("Input is invalid")
+
+def print_invalid_option():
+    print("Invalid option")
