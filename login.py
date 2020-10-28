dc5cc9630beb2c5383bac58d3cd5a45789b177bd
imswebra <eric@imswebra.com>
2020-10-28 09:24:13 -0600

    Update login.py to use new style request_input, cleanup accordingly

--- login.py
+++ login.py
@@ -2,9 +2,9 @@ import database as db
 
 from utils import (
     request_input,
-    print_invalid_input,
     print_invalid_option
 )
+
 
 def login():
     print('To exit program type `exit` at any point')
@@ -20,23 +20,24 @@ def login():
             print("To return to main screen type `back`")
             print('Enter: Id')
             while(True):
-                login_values = request_input(password=True)
+                # login_values will have len of 2 (1 + pass)
+                login_values = request_input(expected_len=1, logout_allowed=False, password=True)
+                if not login_values:
+                    continue
+
                 if login_values[0] == "back":
                     return False, None, None, None
 
-                if len(login_values) == 2:
-                    # Attempt to login
-                    login_success = db.login(
-                        login_values[0], login_values[1]
-                    )
-                    if login_success:
-                        # Check if user is privileged
-                        is_privileged = db.check_privilege(login_values[0])
-                        return True, login_values[0], login_values[1], is_privileged
-                    else:
-                        print("Login failed, please try again")
+                # Attempt to login
+                login_success = db.login(
+                    login_values[0], login_values[1]
+                )
+                if login_success:
+                    # Check if user is privileged
+                    is_privileged = db.check_privilege(login_values[0])
+                    return True, login_values[0], login_values[1], is_privileged
                 else:
-                    print_invalid_input()
+                    print("Login failed, please try again")
 
 
         #Sign up
@@ -44,25 +45,26 @@ def login():
             print("To return to main screen type `back`")
             print('Enter: Id, Name, City')
             while(True):
-                sign_up_values = request_input(password=True)
+                # Sign_up_values will have len of 4 (3 + pass)
+                sign_up_values = request_input(expected_len=3, logout_allowed=False, password=True)
+                if not sign_up_values:
+                    continue
+
                 if sign_up_values[0] == "back":
                     return False, None, None, None
 
-                if len(sign_up_values) == 4:
-                    # Attempt to sign up
-                    sign_up_success = db.sign_up(
-                        sign_up_values[0],
-                        sign_up_values[1],
-                        sign_up_values[2],
-                        sign_up_values[3]
-                    )
+                # Attempt to sign up
+                sign_up_success = db.sign_up(
+                    sign_up_values[0],
+                    sign_up_values[1],
+                    sign_up_values[2],
+                    sign_up_values[3]
+                )
 
-                    if sign_up_success:
-                        return True, sign_up_values[0], sign_up_values[3], False
-                    else:
-                        print("Sign up failed, please try again")
+                if sign_up_success:
+                    return True, sign_up_values[0], sign_up_values[3], False
                 else:
-                    print_invalid_input()
+                    print("Sign up failed, please try again")
 
         # Invalid selection
         else:
