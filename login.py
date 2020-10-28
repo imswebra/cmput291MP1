85e4bccab28777b9448a37d4835e238c89a16791
imswebra <eric@imswebra.com>
2020-10-28 15:19:10 -0600

    Refactor login.py into seperate functions

--- login.py
+++ login.py
@@ -6,7 +6,7 @@ from utils import (
 )
 
 
-def login():
+def login_or_signup():
     print('To exit program type `exit` at any point')
     print('Enter:')
     print('1 for Login')
@@ -15,57 +15,62 @@ def login():
     while (True):
         action = request_input()[0]
 
-        #Login
+        # Login
         if action == '1':
-            print("To return to main screen type `back`")
-            print('Enter: Id')
-            while(True):
-                # login_values will have len of 2 (1 + pass)
-                login_values = request_input(expected_len=1, logout_allowed=False, password=True)
-                if not login_values:
-                    continue
-
-                if login_values[0] == "back":
-                    return False, None, None, None
-
-                # Attempt to login
-                login_success = db.login(
-                    login_values[0], login_values[1]
-                )
-                if login_success:
-                    # Check if user is privileged
-                    is_privileged = db.check_privilege(login_values[0])
-                    return True, login_values[0], login_values[1], is_privileged
-                else:
-                    print("Login failed, please try again")
-
-
-        #Sign up
+            return login()
+        # Sign up
         elif action == '2':
-            print("To return to main screen type `back`")
-            print('Enter: Id, Name, City')
-            while(True):
-                # Sign_up_values will have len of 4 (3 + pass)
-                sign_up_values = request_input(expected_len=3, logout_allowed=False, password=True)
-                if not sign_up_values:
-                    continue
-
-                if sign_up_values[0] == "back":
-                    return False, None, None, None
-
-                # Attempt to sign up
-                sign_up_success = db.sign_up(
-                    sign_up_values[0],
-                    sign_up_values[1],
-                    sign_up_values[2],
-                    sign_up_values[3]
-                )
-
-                if sign_up_success:
-                    return True, sign_up_values[0], sign_up_values[3], False
-                else:
-                    print("Sign up failed, please try again")
-
+            return signup()
         # Invalid selection
         else:
             print_invalid_option(max_option=2)
+
+
+def login():
+    print("To return to main screen type `back`")
+    print("Enter: Id")
+    while (True):
+        # login_values will have len of 2 (1 + pass)
+        login_values = request_input(expected_len=1, logout_allowed=False, password=True)
+        if not login_values:
+            continue
+
+        if login_values[0] == "back":
+            return False, None, None, None
+
+        # Attempt to login
+        login_success = db.login(
+            login_values[0], login_values[1]
+        )
+        if login_success:
+            # Check if user is privileged
+            is_privileged = db.check_privilege(login_values[0])
+            return True, login_values[0], login_values[1], is_privileged
+        else:
+            print("Login failed, please try again")
+
+
+def signup():
+    print("To return to main screen type `back`")
+    print("Enter: Id, Name, City")
+    while(True):
+        # Sign_up_values will have len of 4 (3 + pass)
+        sign_up_values = request_input(expected_len=3, logout_allowed=False, password=True)
+        if not sign_up_values:
+            continue
+
+        if sign_up_values[0] == "back":
+            return False, None, None, None
+
+        # Attempt to sign up
+        sign_up_success = db.sign_up(
+            sign_up_values[0],
+            sign_up_values[1],
+            sign_up_values[2],
+            sign_up_values[3]
+        )
+
+        if sign_up_success:
+            return True, sign_up_values[0], sign_up_values[3], False
+        else:
+            print("Sign up failed, please try again")
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sys import argv, exit
 
 import database as db
 from logged_in import logged_in
-from login import login
+from login import login_or_signup
 
 
 if __name__ == '__main__':
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     is_connected = db.connect(argv[1])
 
     while (is_connected):
-        is_logged_in, uid, pwd, is_privileged = login()
+        is_logged_in, uid, pwd, is_privileged = login_or_signup()
         if is_logged_in:
             logged_in(uid, pwd, is_privileged)
