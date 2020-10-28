521b185189d782581410a3cf589ce539bcfb4051
imswebra <eric@imswebra.com>
2020-10-28 09:18:33 -0600

    Add error messaging directly to request_input, add more helpful invalid input messages

--- utils.py
+++ utils.py
@@ -6,14 +6,32 @@ def split_and_strip(input_val):
     return [x.strip() for x in str(input_val).split(',')]
 
 
-def request_input(password=False):
+def request_input(expected_len=0, logout_allowed=True, password=False):
+    '''Requests input from user.
+    Takes three optional parameters.
+    If expected_len is specificed, this function will handle error messages and
+    and return none when the number inputs recieved does not match expected_len,
+    except for when a keyword such as exit, back, or logout is input. Passwords
+    do not contribute to the number of inputs count.
+    Logout_allowed specifies whether "logout" should be considered a keyword.
+    Password specifies whether the input should prompt for a password after the
+    regular input.
+    '''
     response = input('Input: ')
     values = split_and_strip(response)
 
     if values[0] == "exit":
         exit(0)
 
-    keywords = {"back", "logout"}
+    if logout_allowed:
+        keywords = {"back", "logout"}
+    else:
+        keywords = {"back"}
+
+    if (expected_len > 0 and values[0] not in keywords
+            and len(values) != expected_len):
+        print_invalid_input((expected_len, len(values)))
+        return None
 
     if password and values[0] not in keywords:
         password = getpass()
@@ -23,8 +41,15 @@ def request_input(password=False):
     return values
 
 
-def print_invalid_input():
-    print("Input is invalid")
+def print_invalid_input(len_tuple=None):
+    '''Prints an invalid input message.
+    Takes optional tuple of the form (expected_num_items, received_num_items)
+    for "Expected #, got #" style messages.
+    '''
+    if len_tuple:
+        print("Invalid input, expected", len_tuple[0], "items, got", len_tuple[1])
+    else:
+        print("Input is invalid")
 
 
 def print_invalid_option():
