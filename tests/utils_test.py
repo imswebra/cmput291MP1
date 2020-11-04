c46a8f2516e1af6ed9707ba068ae2d91bf579601
Nayan Prakash <nayanprakash2@gmail.com>
2020-11-04 10:20:45 -0700

    fix deprecated unit test call and add to report

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -6,12 +6,12 @@ class TestDatabase(unittest.TestCase):
     def test_split_and_strip(self):
         value = "this,   that  , there"
         expected = ["this", "that", "there"]
-        self.assertEquals(u.split_and_strip(value), expected)
+        self.assertEqual(u.split_and_strip(value), expected)
 
     def test_keyword_input_validate(self):
-        self.assertEquals(u.keyword_input_validate("/back"), (True, False))
-        self.assertEquals(u.keyword_input_validate("/logout"), (True, True))
-        self.assertEquals(u.keyword_input_validate("other_command"), (False, False))
+        self.assertEqual(u.keyword_input_validate("/back"), (True, False))
+        self.assertEqual(u.keyword_input_validate("/logout"), (True, True))
+        self.assertEqual(u.keyword_input_validate("other_command"), (False, False))
 
     def test_get_indices_range(self):
         mock_results = [
@@ -25,14 +25,14 @@ class TestDatabase(unittest.TestCase):
             ("value8"),
             ("value9"),
         ]
-        self.assertEquals(u.get_indices_range(mock_results), (0, 5))
-        self.assertEquals(u.get_indices_range(mock_results, 1, 6), (6, 9))
-        self.assertEquals(u.get_indices_range(mock_results, 0, 5), (5, 9))
+        self.assertEqual(u.get_indices_range(mock_results), (0, 5))
+        self.assertEqual(u.get_indices_range(mock_results, 1, 6), (6, 9))
+        self.assertEqual(u.get_indices_range(mock_results, 0, 5), (5, 9))
 
     def test_stringify(self):
-        self.assertEquals(u.stringify(None), "N/A")
-        self.assertEquals(u.stringify(Exception("a")), "a")
-        self.assertEquals(u.stringify(Exception("abcdefg"), 5), "ab...")
+        self.assertEqual(u.stringify(None), "N/A")
+        self.assertEqual(u.stringify(Exception("a")), "a")
+        self.assertEqual(u.stringify(Exception("abcdefg"), 5), "ab...")
 
     def test_is_index(self):
         mock_results = [
