08afaacf2ef04d806cc16c19e54cf0a3a2729cb6
imswebra <eric@imswebra.com>
2020-11-04 13:42:38 -0700

    Change submission file extension

--- create-submission.sh
+++ create-submission.sh
@@ -1,4 +1,5 @@
 #/bin/sh
+output_file=prjcode.tgz
 ./gen-report-pdf.sh
-rm -f prjcode.tar.gz
-tar -czf prjcode.tar.gz sql/ tests/database_test.py tests/utils_test.py README.txt Report.pdf database.py logged_in.py login.py main.py utils.py
+rm -f $output_file
+tar -czf $output_file sql/ tests/database_test.py tests/utils_test.py README.txt Report.pdf database.py logged_in.py login.py main.py utils.py
